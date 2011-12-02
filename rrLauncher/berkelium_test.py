50663f28f6feef0be07ddccc74e6a0ef2e573083
triselectif <triselectif@gmx.com>
2011-12-02 15:35:13 +0100

    html : test berkelium alone

---
+++ rrLauncher/berkelium_test.py
@@ -0,0 +1,34 @@
+'''
+Berkelium extension demo
+========================
+
+Check http://github.com/tito/kivy-berkelium for more information.
+You must have berkelium-1.0 extension installed before running the demo
+
+'''
+from kivy.uix.scatter import Scatter
+from kivy.uix.floatlayout import FloatLayout
+from kivy.app import App
+
+from kivy.ext import load
+berkelium = load('berkelium', (1, 1))
+
+urls = (
+    "apps/html/app1/large.html",
+    "apps/html/app1/medium.html",
+    "apps/html/app1/small.html"
+)
+
+class BerkeliumBrowserApp(App):
+    def build(self):
+        root = FloatLayout()
+        size = (600, 600)
+        for url in urls:
+            scatter = Scatter(size=size)
+            bk = berkelium.Webbrowser(url=url, size=size)
+            scatter.add_widget(bk)
+            root.add_widget(scatter)
+        return root
+
+if __name__ == '__main__':
+    BerkeliumBrowserApp().run()
