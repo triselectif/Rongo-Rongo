ab6f2fb19bb96302dd3399a07b4e62326246209d
triselectif <triselectif@gmx.com>
2011-12-02 16:35:07 +0100

    html : berk test

--- rrLauncher/berkelium_test.py
+++ rrLauncher/berkelium_test.py
@@ -14,7 +14,7 @@ from kivy.ext import load
 berkelium = load('berkelium', (1, 1))
 
 #urls = ( 'http://kivy.org','http://www.google.com')
-urls = ('file://apps/html/app1/large.html', 'file://apps/html/app1/medium.html','file://apps/html/app1/small.html')
+urls = ('file:///apps/html/app1/large.html', 'file:///apps/html/app1/medium.html','file:///apps/html/app1/small.html')
 
 class BerkeliumBrowserApp(App):
     def build(self):
